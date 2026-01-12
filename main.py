ee8eee53772650c41a2a4b331d0aba955f2e3e37
Nic <146491853+ic-nay@users.noreply.github.com>
2026-01-12 15:37:11 -0500

    Processes now part of same group and being killed all together! Horray!

--- main.py
+++ main.py
@@ -1,25 +1,31 @@
 import argparse
 import os
+import sys
 import subprocess
 from multiprocessing import Process
 from time import sleep
 
 FFMPEG_COMMAND = "ffmpeg -stream_loop -1 -i /home/nic/test_videos/video_1.mp4 -f rtsp rtsp://localhost:8554/live.sdp"
 
+popen_flags = {}
+
 def main():
+    global popen_flags
+    print(popen_flags)
+    pgid = os.getpgid(os.getpid())
     args = parser.parse_args()
     if not os.path.isdir(args.directory):
         raise argparse.ArgumentError(f"{args.directory} is not a valid directory")
     try:
-        subprocess.Popen("./mediamtx", env={"MTX_RTSPADDRESS": f"localhost:{args.port}"})
+        proc = subprocess.Popen("./mediamtx", env={"MTX_RTSPADDRESS": f"localhost:{args.port}"}, process_group=pgid)
     except:
         try:
-            subprocess.Popen("mediamtx", env={"MTX_RTSPADDRESS": f"localhost:{args.port}"})
+            proc = subprocess.Popen("mediamtx", env={"MTX_RTSPADDRESS": f"localhost:{args.port}"}, process_group=pgid)
         except:
             print("Could not find mediamtx program in same directory as script or as user program")
             exit(1)
     
-    sleep(5)
+    sleep(3)
 
     if (args.output):
         if os.path.isfile(args.output):
@@ -27,12 +33,17 @@ def main():
         output_file = open(args.output, "a")
     for i, file in enumerate(os.scandir(args.directory)):
         if file.is_file():            
-            command = ffmpeg_command(file.path, i, noloop=args.noloop, port=args.port)
+            command = ffmpeg_command(file.path, i, noloop=args.noloop, port=args.port, pgid=pgid)
             if (args.output):
                 output_file.write(f"{command}\n")
 
+    sleep(5)
 
-def ffmpeg_command(file_path:str, iteration:int, noloop:bool=False, port="8554") -> str:
+    input("Press ENTER to end execution...")
+    exit(0)
+
+
+def ffmpeg_command(file_path:str, iteration:int, noloop:bool=False, port="8554", pgid:int=0) -> str:
     command = [
             "ffmpeg", 
             "" if noloop else "-stream_loop",
@@ -47,10 +58,10 @@ def ffmpeg_command(file_path:str, iteration:int, noloop:bool=False, port="8554")
             f"rtsp://localhost:{port}/{iteration}.sdp"
         ]
     try:
-        subprocess.Popen(command)
+        subprocess.Popen(command, process_group=pgid)
     except:
         try:
-            subprocess.Popen(command)
+            subprocess.Popen(command, process_group=pgid)
         except:
             print("Could not find ffmpeg program in same directory as script or as user program")
             exit(1)
@@ -67,4 +78,13 @@ if __name__ == "__main__":
     parser.add_argument("-n", "--noloop", action="store_true", default=False)
     parser.add_argument("-p", "--port", default="8554")
     parser.add_argument("-o", "--output")
+
+    if os.name == 'posix':
+        popen_flags["start_new_session"] = True
+    elif os.name == 'nt':
+        popen_flags["creationflags"] = subprocess.CREATE_NEW_PROCESS_GROUP        
+    else:
+        print("Unsupported OS!")
+        exit(1)
+
     main()
